458d74f6f7cf10a8fe0c7c6b7e47860545c9fa87
Shane Sievers <shanabus@gmail.com>
2020-11-11 22:18:20 -0500

    Updated controller and API to use DI

---
+++ Api/Controllers/MessagesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HelloCrowe.Services;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MessagesController : ControllerBase
+    {        
+        private readonly ILogger<MessagesController> _logger;
+        private readonly IMessageService _messageService;
+
+        public MessagesController(ILogger<MessagesController> logger, IMessageService messageService)
+        {
+            _logger = logger;
+            _messageService = messageService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var message = _messageService.GetMessage();
+
+            return Ok(message);
+        }
+    }
+}
--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,7 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using HelloCrowe.Core;
+using HelloCrowe.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +30,9 @@ namespace Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
             });
+
+            services.AddSingleton<IMessageRepository, MessageRepository>();
+            services.AddSingleton<IMessageService, MessageService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,10 +45,12 @@ namespace Api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
             }
 
-            app.UseHttpsRedirection();
+            // if you want to require HTTPS
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            // we won't go there yet
             // app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
