683b72a971abfa976f2c7c38bccc746ceaeb53cc
Shane Sievers <shanabus@gmail.com>
2020-11-13 00:57:30 -0500

    Progress with env switching console

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
 using HelloCrowe.Core;
+using HelloCrowe.Core.Models;
 using HelloCrowe.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -10,21 +13,44 @@ namespace HelloCrowe.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var serviceProvider = new ServiceCollection()
-                .AddLogging()
-                .AddSingleton<IMessageRepository, MessageRepository>()
-                .AddSingleton<IMessageService, MessageService>()
-                .BuildServiceProvider();
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json", optional: false, reloadOnChange: true);
 
-            var logger = serviceProvider.GetService<ILoggerFactory>()
-                .CreateLogger<Program>();
-            logger.LogDebug("Starting...");
+            var services = new ServiceCollection();    
+
+            if (!string.IsNullOrEmpty(environmentName))
+            {
+                builder.AddJsonFile($"appsettings.{environmentName}.json", optional: false, reloadOnChange: true);
+            }
+
+            var config = builder.AddEnvironmentVariables().Build();
+        
+            var environmentSettings = config.GetSection("MessageSource");
+            services.Configure<MessageSource>(environmentSettings);
+
+            var messageSource = new MessageSource();
+            config.GetSection("MessageSource").Bind(messageSource);
+
+            Console.WriteLine($"Are we using file system messages? {messageSource.UseFiles}");
+            
+            if (messageSource.UseFiles)
+            {
+                services.AddSingleton<IMessageRepository, FileBasedMessageRepository>();
+            } else {
+                services.AddSingleton<IMessageRepository, MessageRepository>();
+            }
+            
+            services.AddSingleton<IMessageService, MessageService>();
+
+
+            var serviceProvider = services.BuildServiceProvider();
 
             var messagService = serviceProvider.GetService<IMessageService>();
-
             var message = messagService.GetMessage();
-
-            Console.WriteLine("Hello World\r\n");
+            Console.WriteLine(message);
 
             Console.WriteLine("Press any key to exit");
             Console.ReadKey(true);
